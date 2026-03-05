48ee679f2324dfc94d08e7aae86336d45b6b17e7
baihaqiramadhani-11 <baihakiramadhani66@gmail.com>
2026-03-05 18:07:07 +0700

    add library and array and elemen

--- BubbleShort_b.cpp
+++ BubbleShort_b.cpp
@@ -17,4 +17,27 @@ void input()
             cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
         )
     ]
-       
+cout << endl;
+cout << "======================" << endl;
+cout << "Masukkan elemen array" << endl;
+cout << "======================" << endl;
+
+for (int i = 0; i < n; i++)
+{
+    cout << "Data ke-" << (i + 1) << ": ";
+    cin >> arr[i];
+}
+void bubbleSort()
+{
+    int pass = 1;
+
+    do
+    {
+
+
+   for (int j = 0; j<= n - 1 - pass; j++)
+   (//step 2
+        if(arr{j}) > arr {j + j})
+        {// step 3
+            int temp;
+            temp = arr }
