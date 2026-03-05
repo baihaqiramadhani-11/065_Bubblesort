6b2239ec09cd26c1b99f061786d548f3c4c812ec
baihaqiramadhani-11 <baihakiramadhani66@gmail.com>
2026-03-05 19:03:53 +0700

    Refactor input and BubbleSortArray

--- BubbleShort_b.cpp
+++ BubbleShort_b.cpp
@@ -2,49 +2,52 @@
 #include <string>
 using namespace std;
 
-int arr[20]; //deklarasi variable global array a dengan ukuran 20
-int n; //deklarasi variable global n untuk menyimpan jumlah elemen yang akan diurutkan 
+int arr[20]; //Deklarasi variable global array a dengan ukuran 20
+int n; //Deklarasi variable global n untuk menyimpan banyaknya elemen pada array
 
 void input()
+{ //Prosedur untuk input data ke dalam array
     while (true)
     {
-        cout << "Masukkan banyaknya elemen pada array: ";
-        cin >> n;
-        if (n <= 20)
-            break;
+        cout << "Masukkan banyaknya elemen pada array: "; // Output ke layar
+        cin >> n;                                         // Input dari pengguna
+        if (n <= 20)                                      // Jika n kurang dari atau sama dengan 20, maka keluar dari loop
+            break;                                        // Keluar dari loop
         else
-        (
-            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
-        )
-    ]
-cout << endl;
-cout << "======================" << endl;
-cout << "Masukkan elemen array" << endl;
-cout << "======================" << endl;
+        {                                                           // Jika n lebih dari 20, maka tampilkan pesan error dan minta input ulang
+            cout << "\nArray dapat memiliki maksimal 20 elemen.\n"; // Output ke layar jika n lebih dari 20
+        }
+    }
+    cout << endl;
+    cout << "\n===================" << endl;
+    cout << "Masukkan Elemen Array" << endl;
+    cout << "===================" << endl;
 
-for (int i = 0; i < n; i++)
-{
-    cout << "Data ke-" << (i + 1) << ": ";
-    cin >> arr[i];
+    for (int i = 0; i < n; i++)
+    {                                           // Looping dengan i dimulai dari 0 hingga n-1
+        cout << "Data ke-" << (i + 1) << ": ";  // Output ke layar
+        cin >> arr[i];                          // Input dari pengguna
+    }
 }
-void bubbleSort()
-{
-    int pass = 1;
+
+void BubbleSortArray()
+{                   // Prosedur untuk mengurutkan array menggunakan algoritma bubble sort
+    int pass = 1;   // Step 1
 
     do
     {
-
-   for (int j = 0; j<= n - 1 - pass; j++)
-   (//step 2
-        if(arr{j}) > arr {j + j})
-        {// step 3
-            int temp;
-            temp = arr[j];
+        for (int j = 0; j <= n - 1 - pass; j++)
+        {                                          // Step 2
+            if (arr[j] > arr[j + 1])
+            {                                      // Step 3
+                int temp;
+                temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
-     }
-     pass += 1;                                 // Step 4
+
+        }
+        pass += 1;                                 // Step 4
 
         cout << "\nPass " << pass - 1 << ": ";
         for (int k = 0; k < n; k++)
@@ -55,6 +58,7 @@ void bubbleSort()
 
     } while (pass <= n - 1);                       // Step 5
 }
+
 void display()
 {
     cout << endl;
@@ -69,7 +73,7 @@ void display()
         {
             cout << " --> ";      // Menampilkan koma setelah setiap elemen kecuali elemen terakhir
         }
-         }
+    }
     cout << endl;
     cout << endl;
 
