2918259effc4a000ab433bdc2c1780b30c8da30c
baihaqiramadhani-11 <baihakiramadhani66@gmail.com>
2026-03-05 17:51:03 +0700

    deklarasi variable

--- BubbleShort_b.cpp
+++ BubbleShort_b.cpp
@@ -2,3 +2,19 @@
 #include <string>
 using namespace std;
 
+int arr[20]; //deklarasi variable global array a dengan ukuran 20
+int n; //deklarasi variable global n untuk menyimpan jumlah elemen yang akan diurutkan 
+
+void input()
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array: ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else
+        (
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+        )
+    ]
+       
